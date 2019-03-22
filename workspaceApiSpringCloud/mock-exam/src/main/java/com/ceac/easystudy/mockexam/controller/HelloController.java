e26da47c1af50062cf0aabf973b121e3c6357e48
13810295026 <1750444100@qq.com>
2019-03-22 11:13:03 +0800

    config server

--- workspaceApiSpringCloud/mock-exam/src/main/java/com/ceac/easystudy/mockexam/controller/HelloController.java
+++ workspaceApiSpringCloud/mock-exam/src/main/java/com/ceac/easystudy/mockexam/controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.ceac.easystudy.mockexam.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
+
+	@Value("${esver}")
+	String esver;
+
 	@GetMapping("/hello/{name}")
 	public String hello(@PathVariable String name) {
-		return "1:" + name;
+		return "1:" + esver + "|" + name;
 	}
 }
