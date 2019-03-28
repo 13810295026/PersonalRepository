22ae3b4adca0ed9b14c9041aad4939213e1d9a08
13810295026 <1750444100@qq.com>
2019-03-28 22:46:13 +0800

    Add Interceptor Add RedisTemplate

--- workspaceApiSpringCloud/mock-exam/src/main/java/com/ceac/easystudy/mockexam/config/RedisConfig.java
+++ workspaceApiSpringCloud/mock-exam/src/main/java/com/ceac/easystudy/mockexam/config/RedisConfig.java
@@ -12,6 +12,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
@@ -39,7 +40,8 @@ public class RedisConfig extends CachingConfigurerSupport {
 		om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
 		om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
 		jackson2JsonRedisSerializer.setObjectMapper(om);
-		template.setValueSerializer(jackson2JsonRedisSerializer);
+		template.setValueSerializer(jackson2JsonRedisSerializer);//设置值（value）的序列化采用Jackson2JsonRedisSerializer
+		template.setKeySerializer(new StringRedisSerializer()); //设置键（key）的序列化采用StringRedisSerializer。
 		template.afterPropertiesSet();
 		return template;
 	}
--- workspaceApiSpringCloud/mock-exam/src/main/java/com/ceac/easystudy/mockexam/controller/HelloController.java
+++ workspaceApiSpringCloud/mock-exam/src/main/java/com/ceac/easystudy/mockexam/controller/HelloController.java
@@ -1,7 +1,12 @@
 package com.ceac.easystudy.mockexam.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	@Autowired
+	StringRedisTemplate stringRedisTemplate;
+
+	@Resource(name = "stringRedisTemplate")
+	ValueOperations<String, String> valOps;
+
 	@Value("${esver}")
 	String esver;
 
-	@GetMapping("/hello/{name}")
-	public String hello(@PathVariable String name) {
-		return "1:" + esver + "|" + name;
+	@GetMapping("/hello/{version:.+}")
+	public String hello(@PathVariable String version) {
+		valOps.increment("mock-client-" + version, 1);//val加1操作
+		return "1:" + esver + "|" + version;
 	}
 }
